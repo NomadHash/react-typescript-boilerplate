ee617a918359cf3592dc53985b2e3b03f4547c6f
nomadHash <badsoap141@gmail.com>
2021-11-25 14:56:10 +0900

    feat : 웹팩 플러그인 구성 및 file,url 로더 추가

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,33 @@
 const path = require('path');
+const os = require('os');
 
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+
+const isDevMode = process.env.NODE_ENV.includes('dev');
+
+const plugins = [
+  new HtmlWebpackPlugin({
+    template: './public/index.html',
+    filename: 'index.html',
+  }),
+  new ForkTsCheckerWebpackPlugin(),
+  new CleanWebpackPlugin(),
+];
+
+if (!isDevMode) {
+  plugins.push(
+    new MiniCssExtractPlugin({
+      filename: 'assets/css/[name].[contenthash].css',
+    }),
+  );
+}
 
 module.exports = {
+  mode: isDevMode ? 'development' : 'production',
   // RESOLVE
   resolve: {
     extensions: ['.ts', '.js', '.tsx', 'jsx'],
@@ -20,13 +43,11 @@ module.exports = {
   //* OUTPUT
   output: {
     path: path.resolve(__dirname, 'build/'),
-    publicPath: './',
   },
 
   //* MODULE
   module: {
     rules: [
-      // For Typescript
       {
         test: /\.(ts|tsx)$/,
         use: [
@@ -42,23 +63,55 @@ module.exports = {
       },
       {
         test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+        use: [isDevMode ? 'style-loader' : MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+      {
+        // write image files under 10k to inline or copy image files over 10k
+        test: /\.(jpg|jpeg|gif|png|svg|ico)?$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 10000,
+              fallback: 'file-loader',
+              name: 'assets/images/[name].[ext]',
+            },
+          },
+        ],
+      },
+      {
+        // write files under 10k to inline or copy files over 10k
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 10000,
+              fallback: 'file-loader',
+              name: 'assets/fonts/[name].[ext]',
+            },
+          },
+        ],
       },
     ],
   },
 
   //* PLUGINS
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './public/index.html',
-      filename: 'index.html',
-    }),
-    // For typescript
-    new ForkTsCheckerWebpackPlugin(),
-    new MiniCssExtractPlugin({
-      filename: 'assets/css/app.css',
-    }),
-  ],
+  plugins,
+
+  //* OPTIMIZATION
+  optimization: {
+    // 압축
+    minimize: !isDevMode,
+    // 미니마이저
+    minimizer: [
+      // 플러그인 인스턴스 생성
+      new CssMinimizerPlugin({
+        // CPU 멀티 프로세서 병렬화 옵션 (기본 값: true)
+        parallel: os.cpus().length - 1,
+      }),
+    ],
+  },
 
   //* DEV_SERVER
   devServer: {
